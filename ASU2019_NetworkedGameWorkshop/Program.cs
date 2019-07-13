1d4d3930bced3e69218fac8f0caf4ed2d680c809
Mohamed <mohamed.xyz@hotmail.com>
2019-07-13 23:10:19 +0200

    feat: empty GameManager & Game Loop

--- ASU2019_NetworkedGameWorkshop/Program.cs
+++ ASU2019_NetworkedGameWorkshop/Program.cs
@@ -13,7 +13,7 @@ namespace ASU2019_NetworkedGameWorkshop {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new GameForm());
         }
     }
 }
---
+++ ASU2019_NetworkedGameWorkshop/controller/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASU2019_NetworkedGameWorkshop.controller {
+    class GameManager {
+        private Timer timer;
+
+        public GameManager() {
+        }
+        public void startTimer() {
+            timer = new Timer();
+            timer.Interval = 50; //Arbitrary: 20 ticks per sec
+            timer.Tick += new EventHandler(gameLoop);
+            timer.Start();
+        }
+
+        private void gameLoop(object sender, EventArgs e) {
+            throw new NotImplementedException();
+        }
+    }
+}
